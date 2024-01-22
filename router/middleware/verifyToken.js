425815fb8c847483a388ee266f397d2a1c3c7eb4
sajjadalidev <iamsajjadali.dev@gmail.com>
2024-01-22 20:39:40 +0500

    Enhancement - User module completed

--- router/middleware/verifyToken.js
+++ router/middleware/verifyToken.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
-    const token = authHeader.split("")[1];
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
       if (err)
         res.status(httpStatus.UNAUTHORIZED).json("You are not authorized!");
@@ -18,11 +18,18 @@ export const verifyToken = (req, res, next) => {
 };
 
 export const verifyTokenAndAuthorization = (req, res, next) => {
-  console.log("🚀 ~ verifyTokenAndAuthorization ~ req:", req);
   verifyToken(req, res, () => {
-    console.log("🚀 ~ verifyToken ~ req:", req.user);
-
     if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed! Only Admin can access this.");
+    }
+  });
+};
+
+export const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
       next();
     } else {
       res.status(403).json("You are not allowed! Only Admin can access this.");
--- router/userRouter.js
+++ router/userRouter.js
@@ -30,4 +30,67 @@ userRouter.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+// Get All Users
+userRouter.get("/", verifyTokenAndAuthorization, async (req, res) => {
+  // to get limited users
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(httpStatus.CREATED).json(users);
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+});
+
+// User stats
+userRouter.get("/stats", verifyTokenAndAuthorization, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(httpStatus.CREATED).json(data);
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error);
+  }
+});
+
+// Get User by Id
+userRouter.get("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(httpStatus.CREATED).json(others);
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+});
+
+// Delete User
+userRouter.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res
+      .status(httpStatus.CREATED)
+      .json({ message: "User has been deleted successfully!" });
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+});
+
 export default userRouter;
